daeb8085a878dc78318b2d79641cdbf5f11720e5
sagar <7208sk@gmail.com>
2021-11-12 18:57:05 +0530

    Creating Table

--- AddressBookSQL/SQLQuery1.sql
+++ AddressBookSQL/SQLQuery1.sql
@@ -1,1 +1,11 @@
 --Welcome To AddressBook System
+create table Address_Book_Table
+(FirstName varchar(100),
+SecondName varchar(100),
+Address varchar(250),
+City varchar(100),
+State varchar(100),
+zip BigInt,
+PhoneNumber BigInt,
+Email varchar(200)
+)
